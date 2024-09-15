84b510529ff39da07a3f8c0e247308b05efece83
Luc Conraads <59870716+LavaLaugh@users.noreply.github.com>
2024-09-15 15:59:54 +0200

    Add files via upload

---
+++ epubtocbz.py
@@ -0,0 +1,44 @@
+import glob
+import os
+import shutil
+import sys
+
+choice = input("Choose 1 for AVIF\nChoose 2 for WEBP")
+
+if choice == "1":
+    format = "avif"
+    quality = "48"
+
+elif choice == "2":
+    format = "webp"
+    quality = "55"
+
+else:
+    print("Wrong input, use any key to exit...")
+    sys.exit(0)
+
+
+print("INFO: Getting all EPUB files...")
+path = glob.glob("*.epub")
+path.sort()
+
+print("INFO: Creating output folder...")
+if not os.path.exists("output"):
+    os.makedirs("output")
+
+index = 1
+for file in path:
+    folder = file.replace(".epub", "")
+    os.makedirs(folder)
+
+    print("INFO: Converting EPUB " + folder + " to zip...")
+    os.system("ebook-convert " + file + " " + folder + ".zip")
+
+    zip = folder + ".zip"
+
+    print("INFO: Converting ZIP " + folder + " to CBZ...")
+    os.system("cbconvert convert --format avif --quality 48 --no-nonimage=true --outdir output/ " + zip)
+
+    index += 1
+    shutil.rmtree(folder)
+    os.remove(zip)
